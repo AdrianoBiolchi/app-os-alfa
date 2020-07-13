3daf98f4f7cde4f6e6257b5c8b629de6fe66f614
Adriano Biolchi <adrianobiolchi@outlook.com>
2020-07-13 17:31:32 -0300

    get results

--- src/controllers/ClientController/index.js
+++ src/controllers/ClientController/index.js
@@ -2,40 +2,16 @@ const Firebird = require("node-firebird");
 const options = require("../../config/database");
 module.exports = {
   async index(req, res) {
-    try {
-      Firebird.attach(options, function (err, db) {
-        console.log(options);
-        try {
-          db.query("SELECT * FROM c000025", function (err, result) {
-            try {
-              if (err) {
-                return res.status(400).send({
-                  error:
-                    "Ocorreu um erro ao tentar fazer a consulta de Clientes. Erro : " +
-                    err,
-                });
-              }
+    Firebird.attach(options, function (err, db) {
+      if (err) throw err;
 
-              if (result != undefined) {
-                return res.send(JSON.stringify(result));
-              } else {
-                res
-                  .status(400)
-                  .send({ error: "Não existem dados para ser retornados" });
-              }
-            } catch {
-              res.status(400).send({ error: "Falha ao buscar dados no banco" });
-            }
-          });
-          db.detach();
-        } catch {
-          console.log("Ocorreu um erro ao gerar o pool conexões");
-        }
+      // db = DATABASE
+      // INSERT BUFFER as BLOB
+      db.query("SELECT NOME FROM c000007", function (err, result) {
+        // IMPORTANT: close the connection
+        res.json(Array(result));
+        db.detach();
       });
-    } catch {
-      res
-        .status(400)
-        .send({ error: "Falha ao carregar os dados de clientes do servidor" });
-    }
+    });
   },
 };
