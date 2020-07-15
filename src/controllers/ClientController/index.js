5d7cc9c25a6b4fd81e33bda4900327cfa0bd433a
Adriano Biolchi <adrianobiolchi@outlook.com>
2020-07-15 11:45:00 -0300

    values

--- src/controllers/ClientController/index.js
+++ src/controllers/ClientController/index.js
@@ -14,10 +14,18 @@ module.exports = {
         if (err) throw err;
         // IMPORTANT: release the pool connection
         // const c = result.decode(result, "win1250");
-        console.log(result);
+        function getValues() {
+          var element = false;
+          for (let index = 0; index < result.length; index++) {
+            //const str = iconv.decode(result[key].NOME, "win1252");
+            element = result[index].NOME.toString();
+            return element;
+          }
+        }
+
         // return res.json(c);
 
-        res.json(result);
+        res.json(getValues());
         db.detach();
       });
     });
