774ae989ee55476b1e63322cc5c444981cd1b4e8
Adriano Biolchi <adrianobiolchi@outlook.com>
2020-07-16 15:40:10 -0300

    Testes

--- src/controllers/ClientController/index.js
+++ src/controllers/ClientController/index.js
@@ -10,22 +10,34 @@ module.exports = {
     pool.get(function (err, db) {
       if (err) throw err;
       // db = DATABASE
-      db.query("SELECT * FROM c000007", function (err, result) {
+      db.query("SELECT * FROM c000007", function (err, result, fields) {
         if (err) throw err;
+
         // IMPORTANT: release the pool connection
         // const c = result.decode(result, "win1250");
-        function getValues() {
-          var element = false;
-          for (let index = 0; index < result.length; index++) {
-            //const str = iconv.decode(result[key].NOME, "win1252");
-            element = result[index].NOME.toString();
-            return element;
-          }
-        }
+        // function getValues() {
+        //   var element = false;
 
         // return res.json(c);
+        console.log(fields.length);
+        // function getData() {
+        //   var data = [];
+        //   var clients = [];
+        //   for (let index = 0; index < result.length; index++) {
+        //     const nameField = fields[index].field;
+        //     const dd = result[index] ? String(result[index][nameField]) : null;
+        //     data.push({ value: dd });
 
-        res.json(getValues());
+        //     for (let i = 0; i < fields.length; i++) {
+        //       data.push({ field: nameField });
+        //     }
+        //     clients.push(data);
+        //   }
+
+        //   return clients;
+        // }
+
+        res.send(String(result));
         db.detach();
       });
     });
