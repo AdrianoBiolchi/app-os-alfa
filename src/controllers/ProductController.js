71767d7a2a9e82a5ade9d3c968c5cdb1aed9093a
Adriano Biolchi <adrianobiolchi@outlook.com>
2020-08-22 12:00:43 -0300

    add new feature

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,32 +3,15 @@ const options = require("../config/database");
 
 module.exports = {
   async index(req, res) {
-    // 5 = the number is count of opened sockets
-    var pool = Firebird.pool(5, options);
-
-    // Get a free pool
-    pool.get(function (err, db) {
-      if (err) throw err;
-
-      // db = DATABASE
-      db.query("SELECT * FROM c000025", function (err, result) {
-        // IMPORTANT: release the pool connection
+    Firebird.attach(options, function (err, db) {
+      if (err)
+        throw err;
+      db.query('SELECT CODIGO, PRODUTO, ESTOQUE FROM c000025', function (err, result) {
+        // IMPORTANT: close the connection
         db.detach();
+        return res.json(result);
       });
     });
-
-    // Destroy pool
-    pool.destroy();
-    // Firebird.attach(options, function (err, db) {
-    //   if (err) throw err;
-
-    //   // db = DATABASE
-    //   db.query("SELECT * FROM c000025", function (err, result) {
-    //     // IMPORTANT: close the connection
-    //     db.detach();
-    //     return res.json(result);
-    //   });
-    // });
   },
 
   async update(req, res) {
